5897ada67fd9d88b084d155bfcf7ccf98eb21a93
user <user@DESKTOP-BN62AV6>
2023-09-18 05:05:35 -0700

    Add project files.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        //int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnImageButton1Clicked(object sender, EventArgs e)
+        {
+            imgBtn1.Source = "reality1.jfif";
+        }
+        private void OnImageButton2Clicked(object sender, EventArgs e)
+        {
+            imgBtn2.Source = "reality2.png";
+        }
+    }
+}
